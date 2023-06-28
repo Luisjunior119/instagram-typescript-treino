24b7e0be91b03879c7ece540bb96487ad6356f82
Luis Santana <luisroyale211@gmail.com>
2023-06-28 13:08:16 -0300

    feat: create data file to mapstories

---
+++ src/stories/data.ts
@@ -0,0 +1,41 @@
+import foto1 from "../assets/luizin.svg"
+import foto2 from "../assets/gab.svg"
+import foto3 from "../assets/rato.svg"
+import foto4 from "../assets/proa.svg"
+import foto5 from "../assets/clau.svg"
+import foto6 from "../assets/pan.svg"
+import foto7 from "../assets/lais.svg"
+
+export const data = [
+
+{image_profile:{ alt: "", url: foto1},
+
+  user_name: "Luisu.u5",
+},
+{image_profile:{ alt: "", url: foto7},
+
+  user_name: "lais.dev",
+},
+{image_profile:{ alt: "", url: foto3},
+
+  user_name: "raaatooooo",
+},
+{image_profile:{ alt: "", url: foto4},
+
+  user_name: "Instituto Proa",
+},
+{image_profile:{ alt: "", url: foto5},
+
+  user_name: "claahh_lopes",
+},
+{image_profile:{ alt: "", url: foto6},
+
+  user_name: "bancopan",
+},
+{image_profile:{ alt: "", url: foto2},
+
+  user_name: "gab.augustoz",
+},
+
+
+]
